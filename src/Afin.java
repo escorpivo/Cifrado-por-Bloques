dc1264e85b611a1a64ef0c80c2b594041f3db1e3
LorenaAlbillos <lalbim00@estudiantes.unileon.es>
2024-05-06 18:32:56 +0200

    Update: cambios

--- src/Afin.java
+++ src/Afin.java
@@ -6,14 +6,194 @@ public class Afin {
 	private int k;
 	
 	public void cifrar(String alfabeto) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Dime la frase a encriptar");
+		String frase = entrada.nextLine();
 		
-		System.out.println("Afin");
+		int[][] matrizC = utilidades.generarMatriz();
 		
-		System.out.println("EL VECTOR X ES = ");
-		int[] x = utilidades.generarVectorX();
-		for(int i = 0; i < x.length; i++) {
-			System.out.print(x[i] + " ");
+        for (int i = 0; i < matrizC.length; i++) {
+            for (int j = 0; j < matrizC[i].length; j++) {
+            	
+                System.out.print(matrizC[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
+		
+        k = matrizC.length;
+        
+		ArrayList<Integer> bloques = new ArrayList<>();
+		for(int i = 0; i < frase.length() ; i++) {
+			
+			bloques.add(utilidades.asignarNumerosAlf(alfabeto, frase.charAt(i)));
 		}
+		
+		for(int i = 0; i < bloques.size(); i++) {
+			System.out.print(bloques.get(i) + " ");
+		}
+		
+		
+		int paddingTotal = (k-(bloques.size() % matrizC[0].length));
+		
+		//solo le hago paddin si hay algun bloque suelto, no tengo que rellenar uno vacio
+		if(paddingTotal < k) {
+			
+			System.out.println("El Padding es: " +  paddingTotal);
+			
+			for(int aux = 0 ; aux < paddingTotal; aux++) {
+				
+				bloques.add(utilidades.generarAleatorios(alfabeto.length()));	
+			}
+		}
+		
+		//añadimos un último bloque con la longitud del mensaje y relleno de 0´s
+		
+		//imprimo la codificación numérica, con el padding y tal
+		System.out.println();
+		for(int i = 0 ; i < bloques.size() ; i++) {
+			
+			System.out.print(bloques.get(i) + " ");
+		}
+		
+		
+		//convertimos el mensaje cifrado a una matriz de longitud K
+		int resto = frase.length()%alfabeto.length();
+		int cociente= frase.length()/alfabeto.length();
+		System.out.println("\nCociente es: " +  cociente + " Y resto es: " + resto);
+		
+		//asigno los valores a la array, en funcion del tamaño de los cocientes
+		int[] arrayCocientes = new int[5];
+		
+		if(cociente > 10000) {
+			
+			arrayCocientes[0] = (cociente / 10000);
+			cociente = cociente - (arrayCocientes[0] * 10000);
+			
+		}else {
+			
+			arrayCocientes[0] = 0;
+		}
+				
+		if(cociente > 1000) {
+			
+			arrayCocientes[1] = (cociente / 1000);
+			cociente = cociente - (arrayCocientes[1] * 1000);
+			
+		}else {
+			
+			arrayCocientes[1] = 0;
+		}
+	
+		if(cociente > 100) {
+			
+			arrayCocientes[2] = (cociente / 100);
+			cociente = cociente - (arrayCocientes[2] * 100);
+			
+		}else {
+			
+			arrayCocientes[2] = 0;
+		}
+				
+		if(cociente > 10) {
+			
+			arrayCocientes[3] = (cociente / 10);
+			cociente = cociente - (arrayCocientes[3] * 10);
+			
+		}else {
+			
+			arrayCocientes[3] = 0;
+		}
+				
+		if(cociente > 0) {
+			
+			arrayCocientes[4] = (cociente);
+			cociente -= cociente;
+		}else {
+			
+			arrayCocientes[4] = 0;
+		}
+		
+		//arrayCocientes[5] = resto;
+		
+		
+		System.out.println("El array de cocientes y tal es: ");
+		for(int i = 0 ; i < arrayCocientes.length ; i++) {
+			
+			System.out.print( arrayCocientes[i]);
+			bloques.add(arrayCocientes[i]);
+		}
+		
+		System.out.println("\nEl array final es: ");
+		for(int i = 0 ; i < bloques.size(); i++) {
+			
+			System.out.print(bloques.get(i) +  " ");
+		}
+		
+		int numRows = bloques.size() / k;
+		int[][] matrizBloques = new int[numRows][k];
+
+		// Rellenar la matriz con valores de 'bloques'
+		for (int i = 0; i < bloques.size(); i++) {
+		    int row = i / k;
+		    int col = i % k;
+		    matrizBloques[row][col] = bloques.get(i);
+		}
+		
+		//cifro el array enterinni
+		int[][] resultado = new int[numRows][k];
+		for (int i = 0; i < numRows; i++) {
+		    for (int j = 0; j < k; j++) {
+		        int sum = 0;
+		        for (int m = 0; m < k; m++) {
+		            sum += matrizBloques[i][m] * matrizC[m][j];
+		        }
+		        resultado[i][j] = sum % alfabeto.length(); // Aplicar modulo por el tamaño del alfabeto
+		    }
+		}
+
+		System.out.println("\nEL RESULTADO DE LA MULTIPLICACIÓN ES = ");
+		for(int i = 0; i < resultado.length; i++) {
+			for(int j = 0; j < resultado[0].length; j++) {
+				System.out.print(resultado[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		//Cojo cada columna de la matriz resultado y lo convierto en vector
+		ArrayList<Integer> posicionesFinales = new ArrayList<Integer>();
+				
+		for(int i = 0; i < resultado.length; i++) {
+			for(int j = 0; j < resultado[0].length; j++) {
+				posicionesFinales.add(resultado[i][j]);
+			}
+		}
+		
+		//Cada k posiciones del resultado las sumo por el vectorX
+		ArrayList<Integer> bloques2 = new ArrayList<Integer>();
+		int[] vectorX = utilidades.generarVectorX();
+		for(int i = 0; i < posicionesFinales.size(); ) {
+			for(int j = 0; j < vectorX.length; j++) {
+				bloques2.add(Math.floorMod(posicionesFinales.get(i) + vectorX[j], alfabeto.length()));
+				i += 1;
+			}
+		}
+				
+		System.out.println("LOS BLOQUES RESULTANTES DE LA SUMA SON = ");
+		for(int i = 0; i < bloques2.size(); i++) {
+			System.out.print(bloques2.get(i) + " ");
+		}
+		
+		StringBuilder mensaje = new StringBuilder();
+		for(int i = 0; i < bloques2.size(); i++) {
+			for(int j = 0; j < alfabeto.length(); j++) {
+				if(bloques2.get(i) == j) {
+					mensaje.append(alfabeto.charAt(j));
+				}
+			}
+		}
+		
+		System.out.println("Finalmente el mensaje cifrado es: " + mensaje);
 		
 	}
 	
--- src/Utilidades.java
+++ src/Utilidades.java
@@ -30,13 +30,13 @@ public class Utilidades {
         		+ "        		[12, 11, 68, 55, 22, 19]";
         */
 		//AFIN
-		/*
+		
 		String matrizString = "[50,    25, 0,  81, 4],\r\n"
 				+ "        	   [10,    39, 19, 67, 51],\r\n"
 				+ "        	   [34,    49, 63, 9,  56],\r\n"
 				+ "            [31,    21, 33, 55, 6],\r\n"
 				+ "            [82,    69, 34, 48, 1]";
-		*/
+		
 		//DATOS LUNES
 		/*
 		//HILL
@@ -47,13 +47,14 @@ public class Utilidades {
         		+ "        		[82,	69,	34,	48,	1]";
 		*/
 		//AFIN
+		/*
 		String matrizString = "[63,    57, 3,  29, 46, 35],\r\n"
 				+ "            [30,    52, 21, 80, 44, 12],\r\n"
 				+ "        	   [37,    23, 53, 60, 16, 56],\r\n"
 				+ "            [77,    11, 82, 74, 46, 53],\r\n"
 				+ "            [33,    56, 81, 72, 37, 37],\r\n"
 				+ "            [12,    11, 68, 55, 22, 19]";
-		
+		*/
 		
         String[] filas = matrizString.split("\r\n");
 
@@ -94,13 +95,13 @@ public class Utilidades {
         		+ "        		[78, 29, 73, 13, 30, 72]";
         */
 		//AFIN
-		/*
+		
 		String matrizInversa = "[80,	76,	37,	53,	50],\r\n"
 				+ "				[44,	45,	23,	54,	33],\r\n"
 				+ "				[81,	76,	27,	68,	12],\r\n"
 				+ "				[61,	59,	47,	44,	67],\r\n"
 				+ "				[10,	31,	17,	20,	80]";
-		*/
+		
 		//DATOS LUNES
 		/*
 		//HILL
@@ -111,12 +112,15 @@ public class Utilidades {
         		+ "        		[10,	31,	17,	20,	80]";
 		*/
 		//AFIN
+		/*
 		String matrizInversa = "[21,	83,	34,	78,	20,	65],\r\n"
 				+ "				[67,	74,	45,	78,	64,	27],\r\n"
 				+ "				[66,	67,	40,	28,	24,	62],\r\n"
 				+ "				[81,	55,	76,	31,	20,	6],\r\n"
 				+ "				[31,	33,	29,	41,	59,	45],\r\n"
 				+ "				[78,	29,	73,	13,	30,	72]";
+		*/
+				
         String[] filas = matrizInversa.split("\r\n");
 
         int[][] matrizNumerica = new int[filas.length][];
